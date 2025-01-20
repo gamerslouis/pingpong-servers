3760bd78c220c250ced7a5624c17204175207669
gamerslouis <gamerslouis@gmail.com>
2025-01-20 14:57:41 +0800

    Enhance logging configuration in Flask application

--- python-flask/main.py
+++ python-flask/main.py
@@ -1,7 +1,24 @@
 from flask import Flask, jsonify, request
 from datetime import datetime
+from logging.config import dictConfig
 
 app = Flask(__name__)
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 @app.route('/ping', methods=['GET'])
 def ping():
